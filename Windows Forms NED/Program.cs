56a1c0f4567ab54ca508ba209980a4e0629ac018
TheCaptainMoo <thecaptainmoo@outlook.com>
2022-11-01 18:26:44 +1000

    Added File Association Support
    .NED files when associated with NED will open with correct data.

--- Windows Forms NED/Program.cs
+++ Windows Forms NED/Program.cs
@@ -6,12 +6,12 @@ namespace Windows_Forms_NED
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Form1(args));
         }
     }
 }
